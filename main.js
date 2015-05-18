66571c11bf6608194aba5d46a0e801c219cb6fa8
Vi Pham <tvpham11@gmail.com>
2015-05-18 19:12:33 -0400

    Converted decimal number to hexadecimal.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var box = document.getElementById('box');
 var clock = document.getElementById('clock');
 
 function checkTime (x) {
@@ -13,19 +12,23 @@ function currentTime () {
   var hours = checkTime(currentDay.getHours());
   var minutes = checkTime(currentDay.getMinutes());
   var seconds = checkTime(currentDay.getSeconds());
-  var hexCode = '#' + hours + minutes + seconds;
+  var timeNoColon = "" + hours + minutes + seconds;
+  var timeConcat = Number(timeNoColon);
+  var fullHex = timeConcat.toString(16);
 
-  clock.innerHTML = hexCode;
-  box.style.backgroundColor = hexCode;
-  clock.style.backgroundColor = hexCode;
+    if (fullHex.length < 6) {
+      fullHex = ("#0" + fullHex).toUpperCase();
+    } else {
+      fullHex = ("#" + fullHex).toUpperCase();
+    }
+
+  clock.innerHTML = fullHex;
+  box.style.backgroundColor = fullHex;
+  clock.style.backgroundColor = fullHex;
 
   var timer = setTimeout(function () {
             currentTime()
         }, 1000);
-}
-
-function stopCurrentTime() {
-  clearInterval(timer);
 }
 
 currentTime();
